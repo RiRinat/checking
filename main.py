ddc0f3494f427562a7e25c22c7466eb4f71b6a8a
RiRinat <140881665+RiRinat@users.noreply.github.com>
2026-02-13 19:02:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,24 +13,9 @@ print(f'выберите доступный язык:')
 for k, v in LANGUAGES.items():
     print(f'{k}: {v}')
 
+
 dest = input('ввод: ')
 
 translator = Translator()
 translations = translator.translate(data, dest=dest)
 print(translations.text)
-
-# print(f'{title} {text} {author} {date}')
-# print(LANGUAGES)
-
-
-# print(translator.translate('안녕하세요.'))
-# print(translator.translate('안녕하세요.', dest='ja'))
-# print(translator.translate('veritas lux mea', src='la'))
-
-# translator = Translator(service_urls=[
-#       'translate.google.com',
-#       'translate.google.co.kr',
-#     ])
-
-# for translation in translations:
-#     print(translation.origin, ' -> ', translation.text)
